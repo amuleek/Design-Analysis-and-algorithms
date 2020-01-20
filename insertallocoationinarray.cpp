2b5db2e6da059f77f8e79c0e0e5d0bc4f0db0fde
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-20 12:33:59 +0530

    Update insertallocoationinarray.cpp
    Programs to insert values of array on random positions and then allocating them into new array.

--- insertallocoationinarray.cpp
+++ insertallocoationinarray.cpp
@@ -18,4 +18,27 @@ int main()
  cin>>position;
  array[position]=number;
  }
- 
+ for(int i=0;i<10;i++)
+ {
+ for(int j=i+1;j<10;j++)
+ {
+ if(array[i]>array[j])
+ {
+ temp=array[i];
+ array[i]=array[j];
+ array[j]=temp;
+ }
+ }
+ }
+ cout<<"Sorted array : ";
+ for(int j=0;j<10+1;j++)
+ {
+ if(array[j]!=NULL)
+ {
+ cout<<array[j]<<" , ";
+ count++;
+ }
+ }
+int *arr = new int[count];
+ return 0;
+}
