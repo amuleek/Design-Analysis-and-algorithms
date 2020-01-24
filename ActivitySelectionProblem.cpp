31d3d1735771aa21c64f6be2bf10faf259affd1c
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-24 10:47:13 +0530

    Update ActivitySelectionProblem.cpp

--- ActivitySelectionProblem.cpp
+++ ActivitySelectionProblem.cpp
@@ -9,3 +9,16 @@ void activity_selection(int start_time[], int finish_time[], int number_of_proce
  {
  if (start_time[j] >= finish_time[i])
  {
+ printf ("%d ", j);
+ i = j;
+ }
+ }
+}
+int main()
+{
+ int start_time[] = {1, 3, 0, 5, 8, 5};
+ int finish_time[] = {2, 4, 6, 7, 9, 9};
+ int number_of_processes = sizeof(start_time)/sizeof(start_time[0]);
+ activity_selection(start_time, finish_time, number_of_processes);
+ return 0;
+}
