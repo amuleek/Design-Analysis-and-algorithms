cc4d3d26ab67c6e1014e4c7a00c10ea97422489e
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-25 22:25:43 +0530

    Update insertionsort.cpp

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -6,3 +6,14 @@ int main()
 	cout<<"Enter Array Size : ";
 	cin>>size;
 	cout<<"Enter Array Elements : ";
+for(i=0; i<size; i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Sorting array using Insertion sort ... \n";
+	for(i=1; i<size; i++)
+	{
+		temp=arr[i];
+		j=i-1;
+		while((temp<arr[j]) && (j>=0))
+		{
