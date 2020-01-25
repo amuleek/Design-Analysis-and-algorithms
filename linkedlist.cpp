1307814c13eaeaa8ac98cedfd8f152f743b3ad92
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-25 22:33:40 +0530

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -14,3 +14,39 @@ void insert_at_beginning(int num)
  if(head == NULL)
  {
  head=t;
+   head->data=num;
+ head->next=NULL;
+ count=1;
+ }
+ else
+ {
+ t->data=num;
+ t->next=head;
+ head=t;
+ count++;
+ }
+}
+void insert_at_end(int num)
+{
+ struct node *t,*temp;
+ t = (struct node*) malloc (sizeof(struct node));
+ if(head == NULL)
+ {
+ head=t;
+ head->data=num;
+ head->next=NULL;
+ count=1;
+ }
+ else
+ {
+ temp=head;
+ while(temp->next!=NULL)
+ {
+ temp=temp->next;
+ }
+ temp->next=t;
+ t->data=num;
+ t->next=NULL;
+ count++;
+ }
+
