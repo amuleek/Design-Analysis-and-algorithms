c6f85484621c64200ed654315f002182db0c928a
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-25 22:20:34 +0530

    Update usestrauctures.cpp

--- usestrauctures.cpp
+++ usestrauctures.cpp
@@ -13,4 +13,15 @@ int main()
 {
  string name;
  struct devops s1,*s2,*s3;
+  s2 = (struct devops*) malloc (sizeof(struct devops));
+ s3 = (struct devops*) malloc (sizeof(struct devops));
+ cout<<"Enter Name of three students ";
+ cin>>name;
+ s1.name=name;
+ cin>>name;
+ s2->name=name;
+ cin>>name;
+ s3->name=name;
+ cout<<"Enter dob(ddmmyyyy) of three students ";
+
 
