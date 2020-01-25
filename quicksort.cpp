ae4036657f22ab758dad7b89801c179b383510af
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-01-25 22:29:08 +0530

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -48,6 +48,20 @@ void displayArray(int arr[], int size)
 
 }
 
+int main()
+{
+    int arr[] = {12,23,3,43,51,35,19,45};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Input array"<<endl;
+    displayArray(arr,n);
+    cout<<endl;
+    quickSort(arr, 0, n-1);
+    cout<<"Array sorted with quick sort"<<endl;
+    displayArray(arr,n);
+    return 0;
+}
+
+
 
 
 
